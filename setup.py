7d94982fe4ca2ffd49440d98977812a67acaff69
Pedro Lacerda <pslacerda@gmail.com>
2013-12-11 03:15:43 -0300

    Added trove classifiers

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ setup (
     name = 'telecorpo',
     version = '0.1',
     packages = find_packages(),
-    install_requires = ['docopt', 'flask', 'flask-restful', 'requests'],
-    # entry_points = {
-    #     'console_scripts': ['tc = tc.cli:main']
-    #     }
+    install_requires = ['docopt', 'flask', 'flask-restful', 'requests', 'coloredlogs'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Multimedia :: Video :: Capture",
+        "Topic :: Multimedia :: Video :: Display",
+        "License :: Other/Proprietary License"
+        ],
 )
