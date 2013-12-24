4f7f407d495dd64c068c7f00fa87bf222f60db34
pslacerda <pslacerda@gmail.com>
2013-12-24 00:56:41 +0000

    removed gtk dependence

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ PYVENV=pyvenv-3.3
 INSTALL_DIR=/opt/telecorpo
 REPOSITORY=https://bitbucket.org/pslacerda/telecorpo.git
 
-apt-get install -y python3.3 python3-enum34 git-core
-apt-get install -y python3-gi gstreamer1.0-tools gir1.2-gstreamer-1.0 \
+apt-get install -y python3.3 python3-enum34 git-core \
+                   python3-gi gstreamer1.0-tools gir1.2-gstreamer-1.0 \
                    gir1.2-gst-plugins-base-1.0 gstreamer1.0-plugins-good \
                    gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad \
                    gstreamer1.0-libav
