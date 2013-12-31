6e276a63625c4067dcfecb78fffb8f8a7198af44
Pedro Lacerda <pslacerda@gmail.com>
2013-12-31 20:25:34 -0300

    Removed tests package.

--- tc/tests.py
+++ tc/tests.py
@@ -1,11 +1,9 @@
-
 from mock import MagicMock
 from twisted.internet import reactor
 from twisted.trial import unittest
 
 
 class TestCase(unittest.TestCase):
-
     def setUp(self):
         reactor.stop = MagicMock()
 
