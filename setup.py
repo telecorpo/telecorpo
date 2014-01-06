8bcbeeea89bbd34f8a9c4b97b62189709f8555b0
Pedro Lacerda <pslacerda@gmail.com>
2014-01-06 14:25:09 -0300

    Removed unused dependencies and add trove classifiers.

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 from setuptools import setup, find_packages
-setup (
+
+setup(
     name = 'telecorpo',
     version = '0.1',
+
+    author = 'Pedro Lacerda',
+    author_email = 'pslacerda+tc@gmail.com',
+    url = 'www.poeticatecnologica.ufba.br',
+    description = 'software suite for telematic dance',
+
     packages = find_packages(),
     scripts = ['bin/tc'],
-    install_requires = ['twisted', 'mock', 'ipython', 'pylint', 'ipdb'],
+    install_requires = ['twisted', 'mock', 'docopt'],
+
     classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: Other/Proprietary License"
+        "Operating System :: POSIX :: Linux",
+
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+
+        "Topic :: Artistic Software",
         "Topic :: Multimedia :: Video",
         "Topic :: Multimedia :: Video :: Capture",
         "Topic :: Multimedia :: Video :: Display",
-        "License :: Other/Proprietary License"
         ],
 )
