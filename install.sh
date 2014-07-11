328fe39fca199317c920c193dc5c2d3435e9c763
Pedro Lacerda <pslacerda@gmail.com>
2014-07-10 23:04:19 -0300

    new installer

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-version=$1
+apt-get install -qq python3 python3-setuptools python3-gi \
+                    gstreamer1.0-{tools,libav,plugins-{good,bad,ugly}} \
+                    gir1.2-{gstreamer-1.0,gtk-3.0}
+
+tag=https://api.bitbucket.org/1.0/repositories/pslacerda/telecorpo/tags
+tag=$(wget -q -O - $tag | python -m json.tool | grep -E '^ {4}\"' \
+    | cut -d\" -f2 | sort -rt_ -k1,1 -k2,2 | head -1)
+
 tempdir=$(mktemp -d)
+tarball=https://bitbucket.org/pslacerda/telecorpo/get/$tag.zip
 
-apt-get install -y python3 python3-setuptools python3-gi \
-                   gstreamer1.0-{tools,libav,plugins-{good,bad,ugly}} \
-                   gir1.2-{gstreamer-1.0,gtk-3.0}
-
-wget -O $tempdir/$version.zip https://bitbucket.org/pslacerda/telecorpo/get/$version.zip
-unzip -d $tempdir $tempdir/$version.zip
-python3 $tempdir/pslacerda-telecorpo*/setup.py install
+echo $tag $tarball
+(cd $tempdir && wget -q $tarball && unzip $tag)
+(cd $tempdir/*/ && python3 setup.py install)
 
 rm -rf $tempdir
