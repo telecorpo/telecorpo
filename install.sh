76d1aad62c8d8738010a06466bcb92444ce1643c
Pedro Lacerda <pslacerda@gmail.com>
2015-10-09 00:21:22 -0300

    instalação simplificada

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ apt-get update -q
 
 echo ${green}[2/4] ${reset}Installing dependencies
 apt-get install -q python3 python3-tk python3-setuptools python3-gi \
-                    gstreamer1.0-{tools,,plugins-{good,bad,ugly}} \
-                    gir1.2-{gstreamer-1.0,gtk-3.0}
+                    gstreamer1.0-{tools,plugins-{good,bad,ugly}} \
+                    gir1.2-{gstreamer-1.0,gtk-3.0,gst-rtsp-server-1.0}
 
 tag=https://api.github.com/repos/pslacerda/telecorpo/tags
 tag=$(wget -q -O - $tag | awk -F\" '/"name"/ {print $4}' \
@@ -24,5 +24,6 @@ echo ${green}[3/4] ${reset}Downloading ${bold}version $tag${reset} and extractin
 
 echo ${green}[4/4] ${reset}Running setuptools
 (cd $tempdir/*/ && python3 setup.py -q install)
+(cd $tempdir/*/ && cp telecorpo.desktop /usr/share/applications)
 
 rm -rf $tempdir
--- setup.py
+++ setup.py
@@ -1,45 +1,11 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-import os
-import shutil
-
-
-class CustomInstallCommand(install):
-
-    def run(self):
-        install.run(self)
-        etcdir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'etc')
-        if platform.machine() == 'x86_64':
-            etcdir = os.path.join(etcdir, 'amd64')
-        else:
-            etcdir = os.path.join(etcdir, 'i386')
-
-        copies = [('/usr/share/gir-1.0', 'GstRtspServer-1.0.gir'),
-                  ('/usr/lib/girepository-1.0', 'GstRtspServer-1.0.typelib'),
-                  ('/usr/lib/', 'libgstrtspserver-1.0.so.0.203.0'),
-                  ('/usr/share/applications', 'telecorpo.desktop')]
-
-        for dst, src in copies:
-            src = os.path.join(etcdir, src)
-            if not os.path.isfile(dst):
-                shutil.copy(src, dst)
-        
-        links = ['libgstrtspserver-1.0.so.0',
-                 'libgstrtspserver-1.0.so']
-        for link in links: 
-            src = '/usr/lib/libgstrtspserver-1.0.so.0.203.0'
-            dst = os.path.join('/usr/lib', link)
-            if not os.path.isfile(dst):
-                os.symlink(src, dst)
 
 setup(
     name = 'telecorpo',
-    version = '0.98',
+    version = '0.99',
     
-    cmdclass = {
-        'install': CustomInstallCommand,
-    },
     packages = find_packages(),
-    scripts = ['scripts/telecorpo'],
+    scripts = ['telecorpo'],
 )
